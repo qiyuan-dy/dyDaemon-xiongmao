d20b5097173c1fe85c090e42f15baf29f5aeb33f
qiyuan <qiyuan@dns2.local>
2026-07-28 20:55:22 +0800

    fix: daemon API从POST+JSON改为GET+query参数匹配旧版daemon

--- XMDaemonClient.m
+++ XMDaemonClient.m
@@ -3,7 +3,7 @@
 //  dyDaemon - 熊猫平台版
 //
 //  Daemon API 客户端实现
-//  daemon 运行在 localhost:12933（CH1/CH2 业务）
+//  daemon 运行在 localhost:12933（CH1/CH2 业务）— GET + query params
 //
 
 #import "XMDaemonClient.h"
@@ -37,54 +37,49 @@ static NSString * const kDaemonHost = @"http://127.0.0.1:12933";
     return self;
 }
 
-#pragma mark - CH1: followUser3
+#pragma mark - CH1: followUser3 (GET /followUser3?user_id=xxx&sec_id=xxx&channel=13)
 
 - (void)ch1_followUser:(NSString *)uid
                 secUid:(NSString *)secUid
             completion:(void (^)(BOOL, NSString *))completion {
     
-    NSString *urlStr = [NSString stringWithFormat:@"%@/followUser3", kDaemonHost];
-    NSDictionary *body = @{
-        @"uid": uid ?: @"",
-        @"sec_uid": secUid ?: @""
-    };
+    NSString *urlStr = [NSString stringWithFormat:@"%@/followUser3?user_id=%@&sec_id=%@&channel=13",
+                        kDaemonHost,
+                        [self urlEncode:uid],
+                        [self urlEncode:secUid]];
     
-    [XMGlobalManager log:@"🔵 CH1 关注: uid=%@", uid];
-    [self post:urlStr body:body completion:completion];
+    [XMGlobalManager log:@"🔵 CH1 daemon关注: uid=%@", uid];
+    [self get:urlStr completion:completion];
 }
 
-#pragma mark - CH2: followUserByLive2
+#pragma mark - CH2: followUserByLive2 (GET /followUserByLive2?user_id=xxx&sec_id=xxx)
 
 - (void)ch2_followUserByLive:(NSString *)uid
                   completion:(void (^)(BOOL, NSString *))completion {
     
-    NSString *urlStr = [NSString stringWithFormat:@"%@/followUserByLive2", kDaemonHost];
-    NSDictionary *body = @{
-        @"uid": uid ?: @""
-    };
+    // CH2 只需 user_id，sec_id 可选（传空也行）
+    NSString *urlStr = [NSString stringWithFormat:@"%@/followUserByLive2?user_id=%@",
+                        kDaemonHost,
+                        [self urlEncode:uid]];
     
-    [XMGlobalManager log:@"🟢 CH2 关注: uid=%@", uid];
-    [self post:urlStr body:body completion:completion];
+    [XMGlobalManager log:@"🟢 CH2 daemon关注: uid=%@", uid];
+    [self get:urlStr completion:completion];
 }
 
-#pragma mark - 通用 POST
+#pragma mark - 通用 GET
 
-- (void)post:(NSString *)urlStr
-        body:(NSDictionary *)body
-  completion:(void (^)(BOOL, NSString *))completion {
+- (void)get:(NSString *)urlStr
+ completion:(void (^)(BOOL, NSString *))completion {
     
     NSURL *url = [NSURL URLWithString:urlStr];
-    NSMutableURLRequest *req = [NSMutableURLRequest requestWithURL:url];
-    req.HTTPMethod = @"POST";
-    [req setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    req.timeoutInterval = 15;
-    
-    NSError *err = nil;
-    req.HTTPBody = [NSJSONSerialization dataWithJSONObject:body options:0 error:&err];
-    if (err) {
-        if (completion) completion(NO, err.localizedDescription);
+    if (!url) {
+        if (completion) completion(NO, @"URL格式错误");
         return;
     }
+    
+    NSMutableURLRequest *req = [NSMutableURLRequest requestWithURL:url];
+    req.HTTPMethod = @"GET";
+    req.timeoutInterval = 15;
     
     NSURLSessionDataTask *task = [self.session dataTaskWithRequest:req
         completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
@@ -105,6 +100,13 @@ static NSString * const kDaemonHost = @"http://127.0.0.1:12933";
                 NSInteger code = [json[@"code"] integerValue];
                 success = (code == 0);
                 msg = json[@"msg"] ?: json[@"status_msg"] ?: @"";
+            } else {
+                // 可能是纯文本响应
+                NSString *text = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                if (text && text.length > 0) {
+                    success = ([text containsString:@"ok"] || [text containsString:@"success"]);
+                    msg = text;
+                }
             }
         }
         
@@ -113,6 +115,13 @@ static NSString * const kDaemonHost = @"http://127.0.0.1:12933";
         });
     }];
     [task resume];
+}
+
+#pragma mark - 辅助
+
+- (NSString *)urlEncode:(NSString *)str {
+    if (!str) return @"";
+    return [str stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]] ?: @"";
 }
 
 @end
